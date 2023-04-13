1b59e9ff0588102a2e681c24b2a2094cabc5e388
maurobrizio <briziomauro2003@gmail.com>
2023-04-13 12:58:25 -0300

    eliminar producto y marca de cantidad listo

--- js/script.js
+++ js/script.js
@@ -6,30 +6,35 @@ const products = [
         id: 1,
         name: "Mate Imperial",
         price: 6500,
+        amount: 1,
         img: "https://d2r9epyceweg5n.cloudfront.net/stores/001/310/957/products/imperial-premium1-dedbd5907665441e1716337116637328-1024-1024.jpg",
     },
     {
         id: 2,
         name: "Yerba Playadito",
         price: 600,
+        amount: 1,
         img: "https://carrefourar.vtexassets.com/arquivos/ids/207097/7793704000928_03.jpg?v=637623985987930000",
     },
     {
         id: 3,
         name: "Termo Stanley",
         price: 20000,
+        amount: 1,
         img: "https://carrefourar.vtexassets.com/arquivos/ids/164142/6939236363136_04.jpg?v=637467710509270000",
     },
     {
         id: 4,
         name: "Matera",
         price: 3500,
+        amount: 1,
         img: "http://cdn.shopify.com/s/files/1/0609/3851/8702/products/matera-cuadrada-cuero_1_grande.png?v=1647359270",
     },
     {
         id: 5,
         name: "Bombilla Pico de Loro",
         price: 1500,
+        amount: 1,
         img: "http://apolomates.com.ar/wp-content/uploads/2016/08/Pico-de-loro-acero-1.jpg",
     },
 ];
@@ -51,12 +56,23 @@ products.forEach((product) => {
     contenido.append(boton);
 
     boton.addEventListener("click", () => {
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+        if (repeat){
+            cart.map((prod) => {
+                if(prod.id === product.id){
+                    prod.amount++;
+                }
+            });
+        }
+        else {
         cart.push({
             id: product.id,
             name: product.name,
             price: product.price,
             img: product.img,
+            amount: product.amount,
         });
+        };
         localCart();
     });
 });
@@ -86,6 +102,7 @@ const takeOut = () => {
         <img src="${product.img}">
         <h3>${product.name}</h3>
         <p>${product.price} $</p>
+        <p>Cantidad: ${product.amount}</p>
         `;
         cartContainer.append(cartContent);
         
@@ -93,6 +110,7 @@ const takeOut = () => {
         close.innerText = "x";
         close.className = "deleteProduct";
         cartContent.append(close);
+        close.addEventListener("click", deleteCartProduct);
 
     });
 
@@ -104,6 +122,13 @@ const takeOut = () => {
 };
 insideCart.addEventListener("click", takeOut);
 
+const deleteCartProduct = () => {
+    const foundId = cart.find((element)=> element.id);
+    cart = cart.filter((cartId) =>{
+        return cartId !== foundId;
+    });
+    takeOut();
+};
 
 const localCart = () => {
     localStorage.setItem("cartStorage", JSON.stringify(cart))
